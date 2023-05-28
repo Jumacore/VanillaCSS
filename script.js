2ff176a1a93ab2e160ed6a4c43b42d027d0ae02e
zm.naufalrio52@gmail.com <zm.naufalrio52@gmail.com>
2023-05-28 09:55:04 +0700

    TambahAnimasi

---
+++ script.js
@@ -0,0 +1,20 @@
+const tombolbtn = document.querySelector(".tombol_btn");
+const tombolbtnIcon = document.querySelector(".tombol_btn i");
+const ddwm = document.querySelector(".ddwm");
+tombolbtn.onclick = function () {
+  ddwm.classList.toggle("open");
+  const isOpen = ddwm.classList.contains("open");
+
+  tombolbtnIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
+};
+
+// const aktif = document.querySelector(".navigation li:nth-child(2) a");
+// const klik2 = document.querySelector(".navigation li a");
+
+// // function klik() {
+
+// // }
+
+// aktif.onclick = function () {
+//   klik2.classList.toggle("aktif");
+// };
