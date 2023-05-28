33fad6e19bc7e8af660c1c13fb9306db441c97e3
zm.naufalrio52@gmail.com <zm.naufalrio52@gmail.com>
2023-05-28 10:29:01 +0700

    ubah function pake adEventListener

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const tombolbtn = document.querySelector(".tombol_btn");
 const tombolbtnIcon = document.querySelector(".tombol_btn i");
 const ddwm = document.querySelector(".ddwm");
-tombolbtn.onclick = function () {
+// tombolbtn.onclick = function () {
+//   ddwm.classList.toggle("open");
+//   const isOpen = ddwm.classList.contains("open");
+
+//   tombolbtnIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
+// };
+
+tombolbtn.addEventListener("click", function () {
   ddwm.classList.toggle("open");
   const isOpen = ddwm.classList.contains("open");
 
   tombolbtnIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
-};
+});
 
 // const aktif = document.querySelector(".navigation li:nth-child(2) a");
 // const klik2 = document.querySelector(".navigation li a");
